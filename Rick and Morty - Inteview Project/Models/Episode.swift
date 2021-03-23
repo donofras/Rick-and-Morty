a97fc7a8a8ea32d4b916c46e89b034e78c4ade15
Denis Onofras <onofras.denisd@gmail.com>
2021-03-23 08:04:10 +0200

    remove unused code

--- Rick and Morty - Inteview Project/Models/Episode.swift
+++ Rick and Morty - Inteview Project/Models/Episode.swift
@@ -20,21 +20,4 @@ struct Episode: Codable {
     let characters: [String]
     let url: String
     let created: String
-
-    enum CodingKeys: String, CodingKey {
-        case id, name, episode, characters, url, created
-        case airDate = "air_date"
-    }
-    
-    func characterIds() -> String {
-        var ids = ""
-        for characterUrl in characters {
-            if let index = characterUrl.range(of: "character/")?.upperBound {
-                let characterIdSubString = (characterUrl[index...])
-                let characterId = String(characterIdSubString)
-                ids.append(characterId + ",")
-            }
-        }
-        return ids
-    }
 }
